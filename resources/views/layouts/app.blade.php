0da41255edee3ef409660720f52163c89cc1302a
Huy Thai <quanghuy9490@gmail.com>
2019-08-21 23:30:37 +0700

    fix styles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -68,7 +68,7 @@
                         <ul class="dropdown-menu">
                                 <!-- User image -->
                                 <li class="user-header">
-                                    <img src="../../dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                                    <img src="{{ URL::to('/') }}/assets/media/avatars/avatar0.jpg" class="img-circle" alt="User Image">
 
                                     <p>
                                         {{ Auth::user()->name }} - {{ Auth::user()->email }}

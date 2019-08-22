687efa56a1b24e1447c44161cfe94d6548cfcbd9
Minh Pham <congminh.pham@mirumagency.com>
2019-08-22 22:20:37 +0700

    migrate users

--- config/infyom/generator_builder.php
+++ config/infyom/generator_builder.php
@@ -5,7 +5,7 @@ return [
     'views' => [
 
         'builder' => 'generator-builder::builder',
-        
+
         'field-template' => 'generator-builder::field-template',
 
         'relation-field-template' => 'generator-builder::relation-field-template'
